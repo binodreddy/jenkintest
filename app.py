55e6a26735ec7182e5a2e765ebaa1606bb73ff16
Binod <binod.reddy@blp.co.in>
2024-07-10 18:37:15 +0530

    final test

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print("hello world")
     return 'Hello, Binod Welcome!!!!!'
+    print("Hello Binod")
 
 if __name__ == '__main__':
     app.run(debug=True)
