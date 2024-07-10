8f6ea863f8a8791a602aefaa298113cba2a57db1
Binod <binod.reddy@blp.co.in>
2024-07-10 17:23:52 +0530

    testing jenkins

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Binod Welcome!!'
+    return 'Hello, Binod Welcome!!!!!'
 
 if __name__ == '__main__':
     app.run(debug=True)
